3e12613f98ecddf52a3887df82645822edd8daac
elvirmoraes-git <elvir.moraes@outlook.com>
2023-05-13 11:11:42 -0300

    Tarefa para disparar erros de digitação de título de texto

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
-public record Todo() {
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public record Todo(String id, String titulo, String descricao, Boolean feito) {
     
+    public Todo {
+        if(titulo == null || titulo.length() < 3){
+            throw new IllegalArgumentException("Um titulo maior que 3 caracteres é necessário");
+        }
+    }
 }
